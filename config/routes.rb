fd62097bbc1ac992282631e99bc59974b683f8cb
kapatsilka <m.kapatsila@gmail.com>
2021-12-27 20:02:05 +0200

    commit for HW 6.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'dogs/index'
+  get 'dogs/show'
+  get 'dogs/new'
+  get 'dogs/edit'
+  resources :dogs
+
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
